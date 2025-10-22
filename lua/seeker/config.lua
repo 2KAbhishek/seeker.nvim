17f6f75d71d423dd55698eee47900dc8a0d82aa6
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2025-10-22 17:39:30 +0530

    feat: cleanup config options

--- lua/seeker/config.lua
+++ lua/seeker/config.lua
@@ -2,20 +2,14 @@
 local M = {}
 
 ---@class SeekerConfigOptions
----@field picker_type string? 'git_files' or 'files' (auto-detect if not specified)
 ---@field toggle_key string? Key to toggle between file and grep mode (default: '<C-e>')
----@field use_git_files boolean? Whether to use git_files (auto-detect if nil)
 ---@field picker_opts table? Options passed to snacks.picker
----@field notifications boolean? Show notifications on mode switch (default: true)
 ---@field add_default_keybindings boolean? Whether to add default keybindings (default: true)
 
 ---@type SeekerConfigOptions
 local config = {
-    picker_type = nil,
     toggle_key = '<C-e>',
-    use_git_files = nil,
     picker_opts = {},
-    notifications = true,
     add_default_keybindings = true,
 }
 
@@ -26,15 +20,6 @@ M.config = config
 ---@param args SeekerConfigOptions?
 M.setup = function(args)
     M.config = vim.tbl_deep_extend('force', M.config, args or {})
-
-    if M.config.use_git_files == nil then
-        local utils = require('seeker.utils')
-        M.config.use_git_files = utils.is_git_repo()
-    end
-
-    if M.config.picker_type == nil then
-        M.config.picker_type = M.config.use_git_files and 'git_files' or 'files'
-    end
 end
 
 ---Get the current configuration
