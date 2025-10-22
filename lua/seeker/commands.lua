a98b7625ec737e002bdd27c7a0ce0298eb1dda5d
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2025-10-22 08:54:28 +0530

    feat: add seeker commands

---
+++ lua/seeker/commands.lua
@@ -0,0 +1,33 @@
+local config = require('seeker.config').config
+
+---@class SeekerCommands
+local M = {}
+
+---Add default keymaps for commands
+local function add_default_keymaps()
+    local function add_keymap(keys, cmd, desc)
+        vim.api.nvim_set_keymap('n', keys, cmd, { noremap = true, silent = true, desc = desc })
+    end
+
+    add_keymap('<leader>sf', ':Seeker<CR>', 'Seeker: Start file investigation')
+end
+
+---Main command handler
+---@param opts table
+local function seeker_command(opts)
+    require('seeker.picker').seek()
+end
+
+---Setup function to initialize commands
+M.setup = function()
+    vim.api.nvim_create_user_command('Seeker', seeker_command, {
+        nargs = 0,
+        desc = 'Start Seeker file investigation',
+    })
+
+    if config.add_default_keybindings then
+        add_default_keymaps()
+    end
+end
+
+return M
